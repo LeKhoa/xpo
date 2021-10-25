4055ccfa30df07cf13609a2cf7bd9721de2ea8b0
vkhoa <khoa.le@flyingwhales.io>
2021-10-25 11:09:29 +0700

    init project, add bootstrap and devise

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    return 'admin' if devise_controller? && resource_name == :admin_user
+    'application'
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+end
