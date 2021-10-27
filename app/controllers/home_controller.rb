1038357365499a4fc1f76e8c5f08131a23a66f17
vkhoa <khoa.le@flyingwhales.io>
2021-10-27 18:08:44 +0700

    Add profile, support page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def dashboard
   end
+
+  def profile
+  end
+
+  def support
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get :profile, to: 'home#profile'
+  get :support, to: 'home#support'
 end
