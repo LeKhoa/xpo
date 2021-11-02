4832fa717bdcfa1cb983f4d809b8d8af9210fb29
vkhoa <khoa.le@flyingwhales.io>
2021-11-02 17:39:56 +0700

    redirect to Onramper widget url

--- app/javascript/components/dashboard/Dashboard.vue
+++ app/javascript/components/dashboard/Dashboard.vue
@@ -11,7 +11,7 @@
                 <path d="M5.89003 7.9C3.62003 7.31 2.89003 6.7 2.89003 5.75C2.89003 4.66 3.90003 3.9 5.59003 3.9C7.37003 3.9 8.03003 4.75 8.09003 6H10.3C10.23 4.28 9.18003 2.7 7.09003 2.19V0H4.09003V2.16C2.15003 2.58 0.590034 3.84 0.590034 5.77C0.590034 8.08 2.50003 9.23 5.29003 9.9C7.79003 10.5 8.29003 11.38 8.29003 12.31C8.29003 13 7.80003 14.1 5.59003 14.1C3.53003 14.1 2.72003 13.18 2.61003 12H0.410034C0.530034 14.19 2.17003 15.42 4.09003 15.83V18H7.09003V15.85C9.04003 15.48 10.59 14.35 10.59 12.3C10.59 9.46 8.16003 8.49 5.89003 7.9Z" fill="#131031"/>
                 </svg>
               </span>
-              <input type="text" class="form-control">
+              <input type="text" class="form-control" v-model="amount">
             </div>
 
             <label class="mt-4"> Payment Method </label>
@@ -21,13 +21,21 @@
                 <path d="M4.42717 8.07895H2.74296V13.9737H4.42717V8.07895ZM9.4798 8.07895H7.79559V13.9737H9.4798V8.07895ZM16.6377 15.6579H0.637695V17.3421H16.6377V15.6579ZM14.5324 8.07895H12.8482V13.9737H14.5324V8.07895ZM8.6377 2.40316L13.0251 4.71053H4.25033L8.6377 2.40316ZM8.6377 0.5L0.637695 4.71053V6.39474H16.6377V4.71053L8.6377 0.5Z" fill="#131031"/>
                 </svg>
               </span>
-              <input type="text" class="form-control">
+              <select class="form-select shadow-none rounded-0" v-model="paymentMethod">
+                <option value="FIAT"> FIAT </option>
+                <option value="CRYPTO"> CRYPTO </option>
+              </select>
             </div>
           </div>
 
           <div class="row actions pt-4 mt-5">
             <div class="col-md-6 col-sm-4 col-6">
-              <button class="btn btn-md bg-purple text-white w-100"> Deposit </button>
+              <a v-if="paymentMethod == 'FIAT'" :href="onRamperWidgetUrl" target="_blank">
+                <button class="btn btn-md bg-purple text-white w-100"> Deposit </button>
+              </a>
+              <a v-else :href="coinPaymentUrl" target="_blank">
+                <button class="btn btn-md bg-purple text-white w-100"> Deposit </button>
+              </a>
             </div>
             <div class="col-md-6 col-sm-4 col-6">
               <button class="btn btn-md bg-purple text-white w-100"> Widthdraw </button>
@@ -132,10 +140,27 @@
 
 <script>
 export default {
-  data: function () {
+  data () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      amount: 500,
+      paymentMethod: 'FIAT',
+      onRamperApiKey: process.env.ONRAMPER_API_KEY,
     }
+  },
+
+  computed: {
+    onRamperWidgetUrl() {
+      return `https://widget.onramper.com?defaultAmount=${this.amount}&apiKey=${this.onRamperApiKey}`;
+    },
+
+    coinPaymentUrl() {
+      return 'https://www.coinpayments.net';
+    }
+  },
+
+  mounted() {
+    console.log(this.onRamperApiKey);
   }
 }
 </script>
