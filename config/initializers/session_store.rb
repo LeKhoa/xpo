e960688723a105d8297091968e6698ebbba08f2f
vkhoa <khoa.le@flyingwhales.io>
2021-11-10 14:06:18 +0700

    session redis storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Xponential::Application.config.session_store :redis_store,
-  servers: ["redis://localhost:6379/0/session"],
+  servers: ["#{ENV['REDIS_URL']}/0/session"],
   expire_after: 90.minutes,
   key: "_#{Rails.application.class.module_parent.name}_session",
   threadsafe: true,
